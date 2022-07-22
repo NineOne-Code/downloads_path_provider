dd407f0527ab48ef2cbe5d067574845ce61b5d39
NineOne <ihim512@gmail.com>
2022-07-22 14:42:35 +0700

    update mainactivity

--- example/android/app/src/main/java/it/nplace/downloadspathproviderexample/MainActivity.java
+++ example/android/app/src/main/java/it/nplace/downloadspathproviderexample/MainActivity.java
@@ -1,13 +1,12 @@
 package it.nplace.downloadspathproviderexample;
 
-import android.os.Bundle;
+
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  //   GeneratedPluginRegistrant.registerWith(this);
+  // }
 }
