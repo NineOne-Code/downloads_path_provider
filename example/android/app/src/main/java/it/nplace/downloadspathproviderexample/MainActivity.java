3ea2099001c0bfdf3bb2995f1bae16904dd9295d
NineOne <ihim512@gmail.com>
2022-07-22 14:39:05 +0700

    update android native

--- example/android/app/src/main/java/it/nplace/downloadspathproviderexample/MainActivity.java
+++ example/android/app/src/main/java/it/nplace/downloadspathproviderexample/MainActivity.java
@@ -1,7 +1,7 @@
 package it.nplace.downloadspathproviderexample;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
